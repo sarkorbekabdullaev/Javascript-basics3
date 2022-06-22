bc97ce3c4cb649149d1ee2fb3e3819adb8a15cbb
Sarkorbek Abdullaev <sarkorbek7777@gmail.com>
2022-06-22 16:18:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+if (6 < 5) {
+    console.log('correct');
+} else {
+    console.log("incorrect");
+}
+
+/* var football = false;
+
+if (football) {
+    document.write('men yoqtiraman')
+} */
+
+var num = 8;
+if (num == 10) {
+    document.write('mening raqamim to`ri');
+} else {
+    document.write('mening raqamim noto`g`ri');
+}
